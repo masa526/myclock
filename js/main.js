de8710c10f192b373b7c5ae95414439f6c3e90ea
masa526 <masa526sk8@gmail.com>
2020-08-26 20:13:31 +0900

    関数の整理

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 'use strict';
 
-{
+(() => {
+    class ClockDrawer {
+        constructor(canvas) {
+            this.ctx = canvas.getContext('2d');
+            this.width = canvas.width;
+            this.height = canvas.height;
+        }
+    }
     class Clock {
-        constructor() {
+        constructor(drawer) {
             this.r = 100;
+            this.drawer = drawer;
         }
         drawFace() {
-            const canvas = document.querySelector('canvas');
-            if (typeof canvas.getContext === "undefined") {
-                return;
-            }
-            const ctx = canvas.getContext('2d');
-            const width = canvas.width;
-            const height = canvas.height;
-
             for (let angle=0; angle < 360; angle+=6){
                 ctx.save();
 
@@ -41,6 +41,12 @@
             this.drawFace();
         }
     }
-    const clock = new Clock();
+
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext === "undefined") {
+        return;
+    }
+
+    const clock = new Clock(new ClockDrawer(canvas));
     clock.run();
-}
+})();
